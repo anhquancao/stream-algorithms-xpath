91159c635d6c479c3a4b91450e270721532333f9
Cao Anh Quan <aquancva@gmail.com>
2018-11-16 15:01:38 +0100

    save

--- src/main/scala/assignment2/Main.scala
+++ src/main/scala/assignment2/Main.scala
@@ -1,17 +1,32 @@
 package assignment2
 
+import scala.collection.mutable
 import scala.io.Source
 
 object Main {
+
+    private var S = mutable.Stack[Array[Int]]()
 
     def processQuery(query: String): Array[String] = {
         val tags = query.split("/")
         tags.slice(1, tags.length).map(x => if (x == "") "*" else x)
     }
 
+    def eval(state: Array[Int], nfa: NFA, tag: String): Array[Int] = {
+        var newState: Array[Int] = state
+
+        for (s <- state) {
+            if (nfa.transitions.keys.exists(_ == (s, tag))) newState = newState :+ nfa.transitions((s, tag))
+        }
+
+        newState
+    }
+
     def main(args: Array[String]): Unit = {
         val filename: String = "data/input.txt"
         val queryStr: String = "//a/b//a"
+
+        var state: Array[Int] = Array(0)
 
         val path = processQuery(queryStr)
 
@@ -19,16 +34,23 @@ object Main {
 
         var nodeId = 0
 
+
         for (line <- Source.fromFile(filename).getLines) {
             val arr = line.split(" ")
             val bit = Integer.parseInt(arr(0))
             val tag = arr(1)
 
+
             if (bit == 0) {
-                //                startElement(tag, path, nodeId)
+                // start elemenent
+                S.push(state)
+                state = eval(state, nfa, tag)
+                if (state.contains(nfa.F)) {
+                    println(nodeId)
+                }
                 nodeId += 1
             } else {
-                //                endElement(tag)
+                state = S.pop()
             }
 
         }
